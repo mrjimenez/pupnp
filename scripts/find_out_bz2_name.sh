0bedbd226c20b4ad42cb523f77d9875f63bb5616
Marcelo Roberto Jimenez <marcelo.jimenez@gmail.com>
2026-02-07 18:25:23 -0300

    find_out_bz2_name.sh: Fix script

--- scripts/find_out_bz2_name.sh
+++ scripts/find_out_bz2_name.sh
@@ -3,11 +3,14 @@
 # If DEBUG is unset, set it to "n"
 : <<< "${DEBUG:=n}"
 
+# Same for TEST_DATA
+: <<< "${TEST_DATA:=n}"
+
 # This array trick is only necessary locally, when
 # there might be some old tar ball lying around.
 #
 # Find the name of the generated tar ball.
-if [[ $DEBUG == n ]]; then
+if [[ $TEST_DATA == n ]]; then
 	# bz2_files=($(ls libupnp-*.tar.bz2))
 	# mapfile -t bz2_files < <(ls libupnp-*.tar.bz2)
 	# Simplest way:
@@ -31,12 +34,15 @@ mapfile -t sorted_bz2_files < <(echo "${bz2_files[*]}" | tr ' ' '\n' | sort -V)
 # Bash arrays are zero based, Zsh arrays are one based.
 # Bash allows negative index.
 bz2_name=${sorted_bz2_files[-1]}
-
 if [[ $DEBUG != n ]]; then
 	echo "bz2_files=\"${bz2_files[*]}\""
 	echo "bz2_name=$bz2_name"
-	tar jcf "${bz2_name}" ./*
 fi
+if [[ -z "${bz2_name}" ]]; then
+	echo "Error: bz2_name is empty"
+	exit 1
+fi
+tar jcf "${bz2_name}" ./*
 
 # Save in the environment
 echo "bz2_name=${bz2_name}" >> "$GITHUB_ENV"
